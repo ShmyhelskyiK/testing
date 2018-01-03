614027a642b9c6d3d030fb11a672c8a755cc3a93
Kostia Shmyhelskyi <k.shmyhelskyi@gmail.com>
2018-01-03 08:14:32 +0200

    added files

---
+++ test/js/script.js
@@ -0,0 +1,60 @@
+// Типы данных, примитывные
+// String - "Hello" 'Hello'
+// Number - 1.2  5
+// Boolean - false, true
+// Null -
+// undefined
+// Symbol
+
+// Типы данных, сложные
+// Object
+// {
+//      name:'Denis',
+//       age:32
+// }
+//Array ['Denis', 'Ivan']
+// Function
+// Date
+
+
+// // переменные
+// // var, let, const
+// var name = 'Kostia1';
+// console.log(name);
+// var name = 'Костя';
+// console.log(name);
+// name = 'Костя123';
+// console.log(name);
+//
+// var init;
+// console.log(init);
+// init = 'Hello';
+// console.log(init);
+//
+// // букв, цифр, _, $
+// // ------------------------------------------
+// // let
+// // let secondName = 'hihi';
+// // secondName ='hi12hi12';
+// // -----------------------------------
+// // const
+// const user = {
+//     name: 'Kostia',
+//     age:'27'
+// };
+// user.name ='Kos';
+// console.log(user);
+// // ---------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
