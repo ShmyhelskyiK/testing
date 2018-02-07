6ba606f5ad6e75d056e808d9e04c07061b47a62b
ShmyhelskyiK <K.Shmyhelskyi@gmail.com>
2018-02-07 23:52:08 +0200

    ++

---
+++ php_test/test.php
@@ -0,0 +1,12 @@
+<?php
+//if(copy('E:\Google Диск\Book\web_design_stiva_kruga.pdf', 'E:\Google Диск\Book\123456789web_design_stiva_kruga.pdf'))
+//{ echo "copy ok"; }
+//else
+//{ echo "failed";}
+//
+
+$fp = fopen("E:\Google Диск\Book\web_design_stiva_kruga.pdf", "r")
+
+?>
+
+$fp
