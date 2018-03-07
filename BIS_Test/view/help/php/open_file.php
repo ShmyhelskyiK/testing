a6d7fd0d15d0169b889e8cc3eeac02a1b02462c0
Kostia Shmyhelskyi <K.Shmyhelskyi@gmail.com>
2018-03-07 16:38:05 +0200

    added

---
+++ BIS_Test/view/help/php/open_file.php
@@ -0,0 +1,11 @@
+<?php
+$file = fopen("\\\\bee.bisbank.com.ua\\USER\\Справочник продавца\\Грошові перекази\\Переводи.pdf","r");
+?>
+
+
+/**
+ * Created by PhpStorm.
+ * User: shmygelskyi
+ * Date: 07.03.2018
+ * Time: 15:09
+ */
