92c3d204bccac02841e3fcbb3fa188d862d63ce9
Kostia Shmyhelskyi <K.Shmyhelskyi@gmail.com>
2018-03-12 17:59:47 +0200

    added

---
+++ BIS_Test/view/bank/php/open_folder_DepGuarFund.php
@@ -0,0 +1,13 @@
+<?php
+$dir = 'explorer.exe \\\bee.bisbank.com.ua\\USER\\Справочник продавца\\ФГВФО';
+exec($dir);
+echo
+"<script language='javascript' type='text/javascript'>
+javascript:history.go(-1);</script>";
+?>
+/**
+ * Created by PhpStorm.
+ * User: administrator
+ * Date: 12.03.2018
+ * Time: 17:40
+ */
