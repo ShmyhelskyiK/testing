b7e41c0eb816bedd4278e4aedc9e71a31893c74d
Kostia Shmyhelskyi <K.Shmyhelskyi@gmail.com>
2018-03-13 18:05:11 +0200

    ++

---
+++ BIS_Test/view/bank/php/download.php
@@ -0,0 +1,23 @@
+<?php
+
+$file_folder = "//bee.bisbank.com.ua\USER\Справочник продавца\ФГВФО"; // папка с файлами
+
+//$file = ("//bee.bisbank.com.ua\USER\Справочник продавца\ФГВФО\Инструкция ФГВФЛ.docx");
+//header ("Content-Type: application/octet-stream");
+//header ("Accept-Ranges: bytes");
+//header ("Content-Length: ".filesize($file));
+//header ("Content-Disposition: attachment; filename=".$file);
+//readfile($file);
+
+//$FullFileName = '//bee.bisbank.com.ua\USER\Справочник продавца\ФГВФО\Инструкция ФГВФЛ.docx';
+//$FlSize = FileSize($FullFileName);
+//If(IsSet($_SERVER['HTTP_USER_AGENT']) and StrPos($_SERVER['HTTP_USER_AGENT'],'MSIE'))
+//    Header('Content-Type: application/force-download');
+//Else
+//    Header('Content-Type: application/octet-stream');
+//Header("Accept-Ranges: bytes");
+//Header("Content-Length: ".$FlSize);
+//Header("Content-Disposition: attachment; filename=Инструкция ФГВФЛ.docx");
+//ReadFile($FullFileName);
+
+?>
