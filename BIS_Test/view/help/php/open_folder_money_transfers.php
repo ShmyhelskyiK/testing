fc91bd3e57597f83ecbdb8772a19a5cd1558f89b
Kostia Shmyhelskyi <K.Shmyhelskyi@gmail.com>
2018-03-15 11:46:49 +0200

    gitignore added

---
+++ BIS_Test/view/help/php/open_folder_money_transfers.php
@@ -0,0 +1,13 @@
+<?php
+$dir = 'explorer.exe \\\bee.bisbank.com.ua\\USER\\Справочник продавца\\ПО SLOLP';
+exec($dir);
+echo
+"<script language='javascript' type='text/javascript'>
+javascript:history.go(-1);</script>";
+?>
+/**
+ * Created by PhpStorm.
+ * User: administrator
+ * Date: 25.02.2018
+ * Time: 19:24
+ */
